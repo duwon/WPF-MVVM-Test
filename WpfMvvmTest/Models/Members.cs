9d3646c1bc4230f139a1e9bac34c8e7166cec76d
정두원 <dwjung@eltronix.co.kr>
2023-07-24 12:10:54 +0900

    Member 모델 구현 및 View에 표시

---
+++ WpfMvvmTest/Models/Members.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMvvmTest.Models
+{
+    public class Member : ObservableObject, ICloneable
+    {
+        public int Id { get; set; }
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+        private string _phone;
+        public string Phone
+        {
+            get { return _phone; }
+            set { SetProperty(ref _phone, value); }
+        }
+        public DateTime RegDate { get; set; }
+        private bool _isUse;
+        public bool IsUse
+        {
+            get { return _isUse; }
+            set { SetProperty(ref _isUse, value); }
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
--- WpfMvvmTest/ViewModels/MainViewModel.cs
+++ WpfMvvmTest/ViewModels/MainViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using WpfMvvmTest.Bases;
+using WpfMvvmTest.Models;
 
 namespace WpfMvvmTest.ViewModels
 {
@@ -13,6 +16,29 @@ namespace WpfMvvmTest.ViewModels
         public MainViewModel() 
         {
             Title = "WPF Mvvm Test";
+            Init();
+        }
+
+        private IList<Member> _member;
+        public IList<Member> Members
+        {
+            get { return _member; }
+            set { SetProperty(ref _member, value); }
+        }
+
+        private void Init()
+        {
+            Members = new ObservableCollection<Member>
+            {
+                new Member
+                {
+                    Id = 1,
+                    Name = "TestName",
+                    Phone = "010-1111-2222",
+                    RegDate = DateTime.Now,
+                    IsUse = true,
+                }
+            };
         }
     }
 }
