a6ae8577dac125a2e42ac39f179d8c2cfa6860b0
정두원 <dwjung@eltronix.co.kr>
2023-07-24 10:52:17 +0900

    프레임워크 사용을 위한 디렉토리 구조 및 파일 생성

--- WpfMvvmTest/Views/MainWindow.xaml.cs
+++ WpfMvvmTest/Views/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfMvvmTest.ViewModels;
 
 namespace WpfMvvmTest
 {
@@ -23,6 +24,7 @@ namespace WpfMvvmTest
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = App.Current.Services.GetService(typeof(MainViewModel));
         }
     }
 }
