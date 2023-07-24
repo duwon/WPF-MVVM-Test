eeba00eb919a84c08da1e25f6c5321d4c95dbbab
정두원 <dwjung@eltronix.co.kr>
2023-07-24 12:03:58 +0900

    폼 제목 Title 바인딩

--- WpfMvvmTest/Bases/ViewModelBase.cs
+++ WpfMvvmTest/Bases/ViewModelBase.cs
@@ -7,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace WpfMvvmTest.Bases
 {
-    internal class ViewModelBase : ObservableObject
+    /// <summary>
+    /// 뷰모델 베이스
+    /// </summary>
+    public abstract class ViewModelBase : ObservableObject
     {
+        private string? _title;
+        /// <summary>
+        /// 타이틀
+        /// </summary>
+        public string? Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
     }
 }
--- WpfMvvmTest/ViewModels/MainViewModel.cs
+++ WpfMvvmTest/ViewModels/MainViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using WpfMvvmTest.Bases;
 
 namespace WpfMvvmTest.ViewModels
 {
-    internal class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
         public MainViewModel() 
         {
+            Title = "WPF Mvvm Test";
         }
     }
 }
