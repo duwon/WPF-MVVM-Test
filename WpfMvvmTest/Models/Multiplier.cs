fe8d415abe2ee8e8480dde257f08583ec14f47d9
정두원 <dwjung@eltronix.co.kr>
2023-07-25 16:22:37 +0900

    페이지로 곱하기 기능 추가

---
+++ WpfMvvmTest/Models/Multiplier.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMvvmTest.Models
+{
+    public class Multiplier : ObservableObject
+    {
+        private int num1 = 1;
+
+        public int Num1
+        {
+            set
+            {
+                SetProperty(ref num1, value);
+                Num2 = value * 2;
+
+            }
+            get
+            {
+                return num1;
+            }
+        }
+
+        private int num2 = 2;
+
+        public int Num2
+        {
+            get
+            {
+                return num2;
+            }
+            set
+            {
+                SetProperty(ref num2, value); 
+            }
+        }
+    }
+}
---
+++ WpfMvvmTest/ViewModels/MultiplierViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMvvmTest.Models;
+
+namespace WpfMvvmTest.ViewModels
+{
+    public class MultiplierViewModel : ObservableObject
+    {
+        private Multiplier? _multiModel = null;
+        public Multiplier? MultiModel
+        {
+            get { return _multiModel; }
+            set { SetProperty(ref _multiModel, value); }
+        }
+
+        public MultiplierViewModel()
+        {
+            MultiModel = new Multiplier();
+
+            PropertyChanged += MultiplierViewModel_PropertyChanged;
+        }
+
+        private void MultiplierViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(MultiModel):
+                    Console.WriteLine(e.PropertyName);
+                    break;
+            }
+        }
+    }
+}
